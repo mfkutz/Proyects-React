11f432d67f7e82deb22445611afcf34735cb84e1
Martin <mfkutz@gmail.com>
2024-10-07 18:45:11 -0300

    types

--- 3-propinas/src/App.tsx
+++ 3-propinas/src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import MenuItems from "./components/MenuItems"
+import { menuItems } from "./data/db"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  console.log(menuItems)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumos</h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2 ">
+        <div>
+          <h2>Menu</h2>
+          {menuItems.map(item => (
+            <MenuItems
+              key={item.id}
+              item={item}
+            />
+          ))}
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
