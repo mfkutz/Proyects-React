2511b8738a8ab6bdd49b6a93a0dda9fa5152da95
Martin <mfkutz@gmail.com>
2024-10-10 22:45:17 -0300

    calories

---
+++ 4-Calorias/src/components/Form.tsx
@@ -0,0 +1,10 @@
+export default function Form() {
+    return (
+        <form>
+            <div>
+                <label htmlFor="">label</label>
+                <select name="" id="">Select</select>
+            </div>
+        </form>
+    )
+}
