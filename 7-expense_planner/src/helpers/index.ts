59dda416c53c1b0766422a62af05ede49880ff56
Martin <mfkutz@gmail.com>
2024-10-15 21:53:44 -0300

    swipe

--- 7-expense_planner/src/helpers/index.ts
+++ 7-expense_planner/src/helpers/index.ts
@@ -1,5 +1,19 @@
+//Format currency
 export function formatCurrency(amount: number) {
   return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(amount);
+}
+
+//Format date
+export function formatDate(dateStr: string): string {
+  const dateObj = new Date(dateStr);
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return new Intl.DateTimeFormat("es-ES", options).format(dateObj);
 }
 
 /* export function formatCurrency(amount: number, currency: string = "USD", locale: string = "en-US") {
