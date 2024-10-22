8fefb2901ae8f23c909368f210cd3a54388326b3
Martin <mfkutz@gmail.com>
2024-10-22 13:02:23 -0300

    after install axios

--- 10-weather-api/src/App.tsx
+++ 10-weather-api/src/App.tsx
@@ -1,13 +1,19 @@
 import styles from "./App.module.css"
 import Form from "./Form/Form"
+import useWeather from "./hooks/useWeather"
 
 function App() {
+  const { fetchWeather } = useWeather()
+
+  // console.log(import.meta.env)
 
   return (
     <>
       <h1 className={styles.title}>Weather</h1>
       <div className={styles.container}>
-        <Form />
+        <Form
+          fetchWeather={fetchWeather}
+        />
         <p>2</p>
       </div>
     </>
--- 10-weather-api/src/Form/Form.tsx
+++ 10-weather-api/src/Form/Form.tsx
@@ -1,9 +1,47 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import type { SearchType } from "../types";
 import { countries } from "../data/country";
 import styles from "../Form/Form.module.css"
+import Alert from "../Alert/Alert";
 
-export default function Form() {
+type FormProps = {
+    fetchWeather: (search: SearchType) => Promise<void>
+}
+
+export default function Form({ fetchWeather }: FormProps) {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: "",
+        country: ""
+    })
+
+    const [alert, setAlert] = useState("")
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        // console.log(search)
+        // console.log([e.target.name], e.target.value)
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (Object.values(search).includes("")) {
+            setAlert("Todos los campos son obligatorios")
+            return
+        }
+
+        fetchWeather(search)
+    }
+
     return (
-        <form className={styles.form}>
+        <form
+            onSubmit={handleSubmit}
+            className={styles.form}
+        >
+            {alert && <Alert>{alert}</Alert>}
             <div className={styles.field}>
                 <label htmlFor="city">Ciudad:</label>
                 <input
@@ -11,12 +49,19 @@ export default function Form() {
                     type="text"
                     name="city"
                     placeholder="Ciudad"
+                    value={search.city}
+                    onChange={handleChange}
                 />
             </div>
 
             <div className={styles.field}>
-                <label htmlFor="city">Pais:</label>
-                <select name="" id="">
+                <label htmlFor="country">Pais:</label>
+                <select
+                    id="country"
+                    value={search.country}
+                    name="country"
+                    onChange={handleChange}
+                >
                     <option value="">--Seleccione un Pais---</option>
                     {countries.map(country => (
                         <option
