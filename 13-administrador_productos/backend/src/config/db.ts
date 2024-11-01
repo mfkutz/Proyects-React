72195e3f6b021b4e24f2bcdd7ed6405339c6af78
Martin <mfkutz@gmail.com>
2024-10-31 21:25:32 -0300

    connecting to PosgreSQL with Sequelize

--- 13-administrador_productos/backend/src/config/db.ts
+++ 13-administrador_productos/backend/src/config/db.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-dotenv.config();
+import { config } from "dotenv";
+config();
 
 const db = new Sequelize(process.env.DATABASE_URL);
 
