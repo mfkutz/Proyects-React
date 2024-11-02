7364f7c304344e9188153f355fe302404602758b
Martin <mfkutz@gmail.com>
2024-11-02 08:50:57 -0300

    dto in routes

--- 13-administrador_productos/backend/src/handlers/product.ts
+++ 13-administrador_productos/backend/src/handlers/product.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
-export const createProduct = async (req: Request, res: Response) => {
-  const product = await Product.create(req.body);
-  res.json({ data: product });
+export const getProduct = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.findAll({
+      order: [["price", "ASC"]],
+    });
+    res.json({ data: products });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createProduct = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const product = await Product.create(req.body);
+    res.json({ data: product });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- 13-administrador_productos/backend/src/router.ts
+++ 13-administrador_productos/backend/src/router.ts
@@ -1,14 +1,26 @@
 import { Router } from "express";
-import { createProduct } from "./handlers/product";
+import { createProduct, getProduct } from "./handlers/product";
+import { body } from "express-validator";
+import { handleInputErrors } from "./middlewares";
 
 const router = Router();
 
 //Routing
-router.get("/", (req, res) => {
-  res.send("Hola mundo en get");
-});
+router.get("/", getProduct);
 
-router.post("/", createProduct);
+router.post(
+  "/",
+  body("name").notEmpty().withMessage("El nombre de Producto no puede ir vacio"),
+  body("price")
+    .notEmpty()
+    .withMessage("El precio no puede ir vacío")
+    .isNumeric()
+    .withMessage("Valor no válido")
+    .custom((value) => value > 0)
+    .withMessage("Precio no válido"),
+  handleInputErrors,
+  createProduct
+);
 
 router.put("/", (req, res) => {
   res.send("Hola mundo en put");
