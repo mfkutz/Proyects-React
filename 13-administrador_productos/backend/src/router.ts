92412282330cc33a905c8eee71e17c174c468dfb
Martin <mfkutz@gmail.com>
2024-11-02 12:39:54 -0300

    testing with jest

--- 13-administrador_productos/backend/src/router.ts
+++ 13-administrador_productos/backend/src/router.ts
@@ -1,12 +1,25 @@
 import { Router } from "express";
-import { createProduct, getProduct } from "./handlers/product";
-import { body } from "express-validator";
+import {
+  createProduct,
+  deleteProduct,
+  getProduct,
+  getProductById,
+  updateAvailability,
+  updateProduct,
+} from "./handlers/product";
+import { body, param } from "express-validator";
 import { handleInputErrors } from "./middlewares";
 
 const router = Router();
 
 //Routing
 router.get("/", getProduct);
+router.get(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
+  handleInputErrors,
+  getProductById
+);
 
 router.post(
   "/",
@@ -22,12 +35,34 @@ router.post(
   createProduct
 );
 
-router.put("/", (req, res) => {
-  res.send("Hola mundo en put");
-});
+router.put(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
+  body("name").notEmpty().withMessage("El nombre de Producto no puede ir vacio"),
+  body("price")
+    .notEmpty()
+    .withMessage("El precio no puede ir vacío")
+    .isNumeric()
+    .withMessage("Valor no válido")
+    .custom((value) => value > 0)
+    .withMessage("Precio no válido"),
+  body("availability").isBoolean().withMessage("Valor para disponibilidad no válido"),
+  handleInputErrors,
+  updateProduct
+);
 
-router.delete("/", (req, res) => {
-  res.send("Hola mundo en delete");
-});
+router.patch(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
+  handleInputErrors,
+  updateAvailability
+);
+
+router.delete(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
+  handleInputErrors,
+  deleteProduct
+);
 
 export default router;
