413a088b91d65a18b2eb6e82c0b32acf302da0f9
Martin <mfkutz@gmail.com>
2024-11-04 10:30:54 -0300

    more test

--- 13-administrador_productos/backend/src/handlers/__tests__/product.test.ts
+++ 13-administrador_productos/backend/src/handlers/__tests__/product.test.ts
@@ -55,5 +55,81 @@ describe("POST /api/products", () => {
 });
 
 describe("GET /api/products", () => {
-  it("GET a JSON response with products", async () => {});
+  it("should check if api/products", async () => {
+    const response = await request(server).get("/api/products");
+    expect(response.status).not.toBe(404);
+  });
+
+  it("GET a JSON response with products", async () => {
+    const response = await request(server).get("/api/products");
+    expect(response.status).toBe(200);
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.body).toHaveProperty("data");
+    expect(response.body.data).toHaveLength(1);
+
+    //not
+    expect(response.body).not.toHaveProperty("errors");
+  });
+});
+
+describe("GET /api/products/:id", () => {
+  it("Should return a 404 response for a non-existent product", async () => {
+    const productId = 2000; //this can change when you need
+    const response = await request(server).get(`/api/products/${productId}`);
+
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty("error");
+    expect(response.body.error).toBe("Producto no entontrado");
+  });
+
+  it("should check a valid ID in the URL", async () => {
+    const response = await request(server).get("/api/products/not-valid-url");
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(1);
+    expect(response.body.errors[0].msg).toBe("ID no válido");
+  });
+});
+
+describe("PUT /api/products/:id", () => {
+  it("should check a valid ID in the URL", async () => {
+    const response = await request(server).put("/api/products/not-valid-url").send({
+      name: "Monitor actualizado 2",
+      price: 350,
+      availability: "true",
+    });
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(1);
+    expect(response.body.errors[0].msg).toBe("ID no válido");
+  });
+
+  it("should display validation error messages", async () => {
+    const response = await request(server).put("/api/products/1").send({});
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toBeTruthy();
+    expect(response.body.errors).toHaveLength(5);
+
+    expect(response.status).not.toBe(200);
+    expect(response.body).not.toHaveProperty("data");
+  });
+
+  it("should validate that the price is greather than 0", async () => {
+    const response = await request(server).put("/api/products/1").send({
+      name: "Monitor actualizado 2",
+      price: -350,
+      availability: "true",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toBeTruthy();
+    expect(response.body.errors).toHaveLength(1);
+    expect(response.body.errors[0].msg).toBe("Precio no válido");
+
+    expect(response.status).not.toBe(200);
+    expect(response.body).not.toHaveProperty("data");
+  });
 });
