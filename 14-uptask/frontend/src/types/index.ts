52ab08e40042aa7e861eadd9060d32efb0d964d6
Martin <mfkutz@gmail.com>
2024-11-13 21:03:00 -0300

    axios code

---
+++ 14-uptask/frontend/src/types/index.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+/* Projects */
+export const projectSchema = z.object({
+  _id: z.string(),
+  projectName: z.string(),
+  clientName: z.string(),
+  description: z.string(),
+  tasks: z.string(),
+});
+
+export type Project = z.infer<typeof projectSchema>;
+export type ProjectFormData = Pick<Project, "clientName" | "projectName" | "description">;
+
+//example with utility type "omit"
+// export type ProjectFormData2 = Omit<Project, "_id">;
