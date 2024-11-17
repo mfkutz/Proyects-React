44dad6a5c6a8fbd506b682ec2dc45b961bccafd6
Martin <mfkutz@gmail.com>
2024-11-17 15:02:31 -0300

    working with axios in taskAPI.ts

--- 14-uptask/frontend/src/components/tasks/TaskCard.tsx
+++ 14-uptask/frontend/src/components/tasks/TaskCard.tsx
@@ -2,6 +2,7 @@ import { Task } from "@/types/index"
 import { Menu, Transition } from "@headlessui/react"
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid"
 import { Fragment } from "react"
+import { useNavigate } from "react-router-dom"
 
 
 type TaskCardProps = {
@@ -9,6 +10,9 @@ type TaskCardProps = {
 }
 
 export default function TaskCard({ task }: TaskCardProps) {
+    //adding path to url (needed on editTaskData)
+    const navigate = useNavigate()
+
     return (
         <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
             <div className=" min-w-0 flex flex-col gap-y-4">
@@ -38,7 +42,11 @@ export default function TaskCard({ task }: TaskCardProps) {
                                 </button>
                             </Menu.Item>
                             <Menu.Item>
-                                <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                                <button
+                                    type='button'
+                                    className='block px-3 py-1 text-sm leading-6 text-gray-900'
+                                    onClick={() => navigate(location.pathname + `?editTask=${task._id}`)}
+                                >
                                     Editar Tarea
                                 </button>
                             </Menu.Item>
