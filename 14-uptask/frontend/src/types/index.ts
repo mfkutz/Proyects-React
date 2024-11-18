5284236f402af02dcab7a15bc81c61978d336faa
Martin <mfkutz@gmail.com>
2024-11-18 20:45:41 -0300

    confirm account

--- 14-uptask/frontend/src/types/index.ts
+++ 14-uptask/frontend/src/types/index.ts
@@ -1,13 +1,19 @@
 import { z } from "zod";
 
+/* Auth & Users */
+export const authSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  password_confirmation: z.string(),
+});
+
+type Auth = z.infer<typeof authSchema>;
+export type UserLoginForm = Pick<Auth, "email" | "password">;
+export type UserRegistrationForm = Pick<Auth, "name" | "email" | "password" | "password_confirmation">;
+
 /* Tasks */
-export const taskStatusSchema = z.enum([
-  "completed",
-  "pending",
-  "onHold",
-  "inProgress",
-  "underReview",
-]);
+export const taskStatusSchema = z.enum(["completed", "pending", "onHold", "inProgress", "underReview"]);
 
 export type TaskStatus = z.infer<typeof taskStatusSchema>;
 
