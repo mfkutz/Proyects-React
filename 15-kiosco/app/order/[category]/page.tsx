09d898aecfdae2a7b035da7729e0fe497cc9e5d6
Martin <mfkutz@gmail.com>
2024-11-29 23:18:28 -0300

    kiosco

---
+++ 15-kiosco/app/order/[category]/page.tsx
@@ -0,0 +1,36 @@
+import { prisma } from '@/src/lib/prisma'
+import ProductCard from '../products/ProductCard'
+
+async function getProducts(category: string) {
+    const products = await prisma.product.findMany({
+        where: {
+            category: {
+                slug: category
+            }
+        }
+    })
+
+    return products
+}
+
+export default async function OrderPage({ params }: { params: { category: string } }) {
+
+    const products = await getProducts(params.category)
+
+    return (
+        <>
+            <h1 className='text-2xl my-10' >
+                Elige y personaliza tu pedido a continuación
+            </h1>
+            <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start'>
+                {products.map(product => (
+                    <ProductCard
+                        key={product.id}
+                        product={product}
+                    />
+                ))
+                }
+            </div>
+        </>
+    )
+}
